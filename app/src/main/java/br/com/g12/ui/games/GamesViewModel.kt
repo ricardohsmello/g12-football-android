537e154ea761ae0c7f0657be9f2c45a9258f9ce8
Ricardo <ricardohsmello@gmail.com>
2025-10-07 00:56:24 -0300

    Including charts

---
+++ app/src/main/java/br/com/g12/ui/games/GamesViewModel.kt
@@ -0,0 +1,13 @@
+package br.com.g12.ui.games
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GamesViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
